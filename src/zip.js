32a5da60545e4702ccb9b16569224f76ad2ee887
paulcoding810 <41385034+longnghia@users.noreply.github.com>
2025-02-08 23:07:41 +0700

    update artifacts in ci

--- src/zip.js
+++ src/zip.js
@@ -1,10 +1,13 @@
 import gulp from 'gulp'
 import zip from 'gulp-zip'
 import { createRequire } from 'module'
+import { loadEnv } from 'vite'
 const require = createRequire(import.meta.url)
 const manifest = require('../build/manifest.json')
 
+const browser = loadEnv('production', process.cwd()).VITE_BROWSER ?? 'chrome'
+
 gulp
   .src('build/**')
-  .pipe(zip(`${manifest.name.replaceAll(' ', '-')}-${manifest.version}.zip`))
+  .pipe(zip(`${manifest.name.replaceAll(' ', '-')}-${browser.case}-${manifest.version}.zip`))
   .pipe(gulp.dest('package'))
