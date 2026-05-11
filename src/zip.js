9703ce98b7e813a059c35c37e9de64d911177724
paulcoding810 <longnghia2.00@gmail.com>
2026-05-11 14:34:55 +0700

    Fix zip command

--- src/zip.js
+++ src/zip.js
@@ -2,11 +2,11 @@ import gulp from 'gulp'
 import zip from 'gulp-zip'
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
-const manifest = require('../build/manifest.json')
 const browser = process.env.BROWSER ?? 'chrome'
+const manifest = require(`../build/${browser}/manifest.json`)
 const name = browser[0].toUpperCase() + browser.slice(1)
 
 gulp
-  .src('build/**')
+  .src(`build/${browser}/**`)
   .pipe(zip(`${manifest.name.replaceAll(' ', '-')}-${name}-${manifest.version}.zip`))
   .pipe(gulp.dest('package/'))
